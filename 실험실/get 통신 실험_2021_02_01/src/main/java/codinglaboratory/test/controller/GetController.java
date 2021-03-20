b1e31f96c15be663dc6cea1ca94719fba377d306
dohyeon5626 <kodohyeon71@gmail.com>
2021-03-20 16:03:52 +0900

    2021_03_20_테스트코드 추가

--- 실험실/get 통신 실험_2021_02_01/src/main/java/codinglaboratory/test/controller/GetController.java
+++ 실험실/get 통신 실험_2021_02_01/src/main/java/codinglaboratory/test/controller/GetController.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GetController {
     private final AtomicLong counter = new AtomicLong();
     @GetMapping("/get")
-    public Get get_return (@RequestParam(value = "value", defaultValue = "현재 서버로 올라온 값이 없음") String value){
+    public Get getReturn (@RequestParam(value = "value", defaultValue = "현재 서버로 올라온 값이 없음") String value){
         return new Get(counter.incrementAndGet(), value);
     }
 }
---
+++ 실험실/get 통신 실험_2021_02_01/src/test/java/codinglaboratory/test/controller/GetControllerTest.java
@@ -0,0 +1,29 @@
+package codinglaboratory.test.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = GetController.class)
+public class GetControllerTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Test
+    public void GetControllerRetrunTest() throws Exception {
+        mvc.perform(get("/get")
+                .param("value", "hello"))
+                .andExpect(status().isOk());
+    }
+}
