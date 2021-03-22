6bbf5bd4ec9001038fe4f5061966b39948302fd0
dohyeon5626 <kodohyeon71@gmail.com>
2021-03-22 10:26:43 +0900

    2021_03_22_테스트코드 추가

--- 실험실/post 통신 실험_2021_02_03/src/main/java/codinglaboratory/test/controller/PostController.java
+++ 실험실/post 통신 실험_2021_02_03/src/main/java/codinglaboratory/test/controller/PostController.java
@@ -11,6 +11,7 @@ public class PostController {
     private final AtomicLong counter = new AtomicLong();
     @PostMapping("/post")
     public Post post_return (@RequestBody Param param){
+        System.out.println(param.getData());
         if(param.getData() == "")
             param.setData("현재 입력된 값이 없습니다");
         return new Post(counter.incrementAndGet(), param.getData());
--- 실험실/post 통신 실험_2021_02_03/src/main/java/codinglaboratory/test/dto/Param.java
+++ 실험실/post 통신 실험_2021_02_03/src/main/java/codinglaboratory/test/dto/Param.java
@@ -3,6 +3,11 @@ package codinglaboratory.test.dto;
 public class Param {
     private String data;
 
+    public Param(String data){
+        this.data = data;
+    }
+
+    public Param() {}
     public String getData() {
         return data;
     }
