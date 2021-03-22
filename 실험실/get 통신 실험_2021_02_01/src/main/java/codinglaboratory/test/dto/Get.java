0384cc7bc655965a7244fdb8d61cd716a5ba9ecf
dohyeon5626 <kodohyeon71@gmail.com>
2021-03-22 11:17:44 +0900

    2021_03_22_테스트 코드 변경

--- 실험실/get 통신 실험_2021_02_01/src/main/java/codinglaboratory/test/dto/Get.java
+++ 실험실/get 통신 실험_2021_02_01/src/main/java/codinglaboratory/test/dto/Get.java
@@ -2,8 +2,18 @@ package codinglaboratory.test.dto;
 
 public class Get {
 
-    private final long id;
-    private final String content;
+    private long id;
+    private String content;
+
+    public Get(){}
+
+    public Get(long id){
+        this.id = id;
+    }
+
+    public Get(String content){
+        this.content = content;
+    }
 
     public Get(long id, String content) {
         this.id = id;
--- 실험실/get 통신 실험_2021_02_01/src/test/java/codinglaboratory/test/controller/GetControllerTest.java
+++ 실험실/get 통신 실험_2021_02_01/src/test/java/codinglaboratory/test/controller/GetControllerTest.java
@@ -1,11 +1,15 @@
 package codinglaboratory.test.controller;
 
+import codinglaboratory.test.dto.Get;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
@@ -19,11 +23,16 @@ public class GetControllerTest {
 
     @Autowired
     protected MockMvc mvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void GetControllerRetrunTest() throws Exception {
-        mvc.perform(get("/get")
+        MvcResult result = mvc.perform(get("/get")
                 .param("value", "hello"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andReturn();
+        Get response = objectMapper.readValue(result.getResponse().getContentAsString(), Get.class);
+        Assertions.assertEquals(response.getContent(), "hello");
     }
 }
