481c152b73dff1678a14f67cbafcaf0184fdc8a1
dohyeon5626 <kodohyeon71@gmail.com>
2021-05-04 21:41:18 +0900

    2021_05_04_코드 추가

--- Fun/crawling_2021_05_03/Dms_Crawling.py
+++ Fun/crawling_2021_05_03/Dms_Crawling.py
@@ -10,23 +10,32 @@ driver.get(url)
 html = driver.page_source
 soup = BeautifulSoup(html)
 
+data = soup.select(".meal--menu--title--date")[0].get_text()
+data = data.replace("\n", " ")
+data = data.replace("\t", " ")
+data = data.replace("            ", " ")
+
 content = soup.select(".meal--card--content--wrapper")
 
 morning = content[0].select(".meal--card--content")
 lunch = content[1].select(".meal--card--content")
 dinner = content[2].select(".meal--card--content")
 
-print("[DSM 아침 급식]---------------------------------")
+print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+print("■ " + data + "급식에 대해서 알려드리겠습니다 ■")
+print("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+
+print("[DSM 아침 급식]-----------------------------------------")
 for i in morning:
     print("■ " + i.get_text())
 print()
 
-print("[DSM 점심 급식]---------------------------------")
+print("[DSM 점심 급식]-----------------------------------------")
 for i in lunch:
     print("■ " + i.get_text())
 print()
 
-print("[DSM 저녁 급식]---------------------------------")
+print("[DSM 저녁 급식]-----------------------------------------")
 for i in dinner:
     print("■ " + i.get_text())
 print()
